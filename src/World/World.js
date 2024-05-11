78e59949482656b8a12d3b8eabd334e768bcf3bb
Yangyang SHI <syy2018@gmail.com>
2024-05-11 14:56:57 +0800

    sudden change position

--- src/World/World.js
+++ src/World/World.js
@@ -6,8 +6,14 @@ import { createLight } from "./components/Light.js";
 import { createRenderer } from "./systems/renderer.js";
 import { Resizer } from "./systems/resizer.js";
 import { createControls } from "./systems/cameraControls.js";
-import { highlightPolygons, removePreviousGeometries } from "./systems/highlightRegion.js";
-import { calculatePolygonCentroid, latLngTo3DPosition } from "./utils/geoUtils.js";
+import {
+  highlightPolygons,
+  removePreviousGeometries,
+} from "./systems/highlightRegion.js";
+import {
+  calculatePolygonCentroid,
+  latLngTo3DPosition,
+} from "./utils/geoUtils.js";
 
 const MARGIN = 24; // Margin in units on each side
 const MAX_EARTH_RADIUS = 100;
@@ -18,7 +24,10 @@ let resizer;
 class World {
   constructor(container) {
     const containerWidth = container.clientWidth;
-    this.earthRadius = Math.min((containerWidth - MARGIN * 2) / 2, MAX_EARTH_RADIUS);
+    this.earthRadius = Math.min(
+      (containerWidth - MARGIN * 2) / 2,
+      MAX_EARTH_RADIUS
+    );
 
     this.camera = createCamera(container);
     this.scene = createScene();
@@ -34,7 +43,17 @@ class World {
     controls = createControls(this.camera, container);
     resizer = new Resizer(container, this.camera, this.renderer);
 
-    this.previousTarget = new THREE.Vector3(0, 0, 1);
+    this.previousTargetLatLng = { lat: 0, lng: -90 };
+    this.resetPosition();
+  }
+
+  resetPosition() {
+    const [x, y, z] = latLngTo3DPosition(0, -90, this.earthRadius);
+    const direction = new THREE.Vector3(x, y, z).normalize();
+    this.earth.quaternion.setFromUnitVectors(
+      new THREE.Vector3(0, 0, 1),
+      direction
+    );
   }
 
   async fetchGeoJson(relativePath) {
@@ -53,83 +72,195 @@ class World {
   async showCountry(countryCca2, style = "pin") {
     const geoJsonPath = `/src/World/assets/country/${countryCca2}.json`;
     const geoJsonData = await this.fetchGeoJson(geoJsonPath);
-    this.highlightRegion(geoJsonData, style);
-  }
 
-  highlightRegion(geoJsonCountryData, style = "pin", elevation = 1.0) {
     removePreviousGeometries(this.earth);
 
     setTimeout(() => {
       this.earth.scale.set(1, 1, 1);
-      highlightPolygons(geoJsonCountryData, this.earth, this.earthRadius, style, elevation);
-      const firstFeature = geoJsonCountryData.features[0];
+      const firstFeature = geoJsonData.features[0];
       const centroid = calculatePolygonCentroid(firstFeature.geometry);
+      const targetLatLng = { lat: centroid.lat, lng: centroid.lng };
 
-      const [x, y, z] = latLngTo3DPosition(centroid.lat, centroid.lng, this.earthRadius);
-      const targetPosition = new THREE.Vector3(x, y, z);
-
-      this.rotateGlobeTo(targetPosition);
+      this.rotateGlobeTo(targetLatLng, () => {
+        highlightPolygons(geoJsonData, this.earth, this.earthRadius, style);
+      });
     }, 200);
   }
 
-  rotateGlobeTo(targetPosition) {
-    const targetDirection = targetPosition.clone().normalize();
+  getZone(lat) {
+    if (lat >= 50) {
+      return 3;
+    } else if (lat >= 40) {
+      return 2;
+    } else if (lat >= 30) {
+      return 1;
+    } else if (lat <= -50) {
+      return -3;
+    } else if (lat <= -40) {
+      return -2;
+    } else if (lat <= -30) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 
-    // Adjust target direction if needed
-    if (targetDirection.x < 0) {
-      targetDirection.x += 0.5;
-    } else if (targetDirection.x > 0) {
-      targetDirection.x -= 0.5;
+  determinePolarRotation = (initialLat, targetLat) => {
+    const initialZone = this.getZone(initialLat);
+    const targetZone = this.getZone(targetLat);
+
+    if (initialZone == targetZone) {
+      return 0; // North to North
     }
 
-    // Use the previous target direction as the starting point for rotation
-    const initialDirection = this.previousTarget.clone().normalize();
-
-    // Calculate the azimuthal angle difference
-    const initialAzimuthalAngle = Math.atan2(initialDirection.z, initialDirection.x);
-    const targetAzimuthalAngle = Math.atan2(targetDirection.z, targetDirection.x);
-
-    const azimuthalRotation = targetAzimuthalAngle - initialAzimuthalAngle;
-
-    // Create a quaternion for azimuthal rotation only
-    const azimuthalQuaternion = new THREE.Quaternion().setFromAxisAngle(
-      new THREE.Vector3(0, 1, 0), // Rotate around y-axis
-      azimuthalRotation
-    );
-
-    // // Determine z-axis rotation to adjust tilting
-    // const zRotation = -targetDirection.y;
-
-    // const zQuaternion = new THREE.Quaternion().setFromAxisAngle(
-    //   new THREE.Vector3(0, 0, 1), // Rotate around z-axis
-    //   zRotation
-    // );
-
-    // // Combine all rotations
-    // const combinedRotation = azimuthalQuaternion.multiply(zQuaternion);
-
-    // Apply the rotation to the Earth using a smooth animation
-    const initialRotation = this.earth.quaternion.clone();
-    const targetRotation = initialRotation.clone().multiply(azimuthalQuaternion);
-
-    let t = 0;
-    const duration = 1.5; // Duration of the animation in seconds
-    const clock = new THREE.Clock();
-
-    const animateRotation = () => {
-      t += clock.getDelta() / duration;
-      this.earth.quaternion.slerp(targetRotation, t);
-
-      if (t < 1) {
-        requestAnimationFrame(animateRotation);
-      } else {
-        // Update the previous target to the current one
-        this.previousTarget.copy(targetPosition);
+    if (initialZone == 3) {
+      if (targetZone == 2) {
+        return THREE.MathUtils.degToRad(-10); // North 50 to North 40
+      } else if (targetZone == 1) {
+        return THREE.MathUtils.degToRad(-20); // North 50 to North 40
+      } else if (targetZone == 0) {
+        return THREE.MathUtils.degToRad(-50); // North 50 to North 40
+      } else if (targetZone == -1) {
+        return THREE.MathUtils.degToRad(-80); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(-90); // North 50 to North 40
       }
+    }
+
+    if (initialZone == 2) {
+      if (targetZone == 3) {
+        return THREE.MathUtils.degToRad(10); // North 50 to North 40
+      } else if (targetZone == 1) {
+        return THREE.MathUtils.degToRad(-10); // North 50 to North 40
+      } else if (targetZone == 0) {
+        return THREE.MathUtils.degToRad(-40); // North 50 to North 40
+      } else if (targetZone == -1) {
+        return THREE.MathUtils.degToRad(-70); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(-80); // North 50 to North 40
+      }
+    }
+
+    if (initialZone == 1) {
+      if (targetZone == 3) {
+        return THREE.MathUtils.degToRad(20); // North 50 to North 40
+      } else if (targetZone == 2) {
+        return THREE.MathUtils.degToRad(10); // North 50 to North 40
+      } else if (targetZone == 0) {
+        return THREE.MathUtils.degToRad(-30); // North 50 to North 40
+      } else if (targetZone == -1) {
+        return THREE.MathUtils.degToRad(-60); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(-70); // North 50 to North 40
+      }
+    }
+
+    if (initialZone == 0) {
+      if (targetZone == 3) {
+        return THREE.MathUtils.degToRad(50); // North 50 to North 40
+      } else if (targetZone == 2) {
+        return THREE.MathUtils.degToRad(40); // North 50 to North 40
+      } else if (targetZone == 1) {
+        return THREE.MathUtils.degToRad(30); // North 50 to North 40
+      } else if (targetZone == -1) {
+        return THREE.MathUtils.degToRad(-30); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(-40); // North 50 to North 40
+      }
+    }
+
+    if (initialZone == -1) {
+      if (targetZone == 3) {
+        return THREE.MathUtils.degToRad(80); // North 50 to North 40
+      } else if (targetZone == 2) {
+        return THREE.MathUtils.degToRad(70); // North 50 to North 40
+      } else if (targetZone == 1) {
+        return THREE.MathUtils.degToRad(60); // North 50 to North 40
+      } else if (targetZone == 0) {
+        return THREE.MathUtils.degToRad(30); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(-10); // North 50 to North 40
+      }
+    }
+
+    if (initialZone == -2) {
+      if (targetZone == 3) {
+        return THREE.MathUtils.degToRad(90); // North 50 to North 40
+      } else if (targetZone == 2) {
+        return THREE.MathUtils.degToRad(80); // North 50 to North 40
+      } else if (targetZone == 1) {
+        return THREE.MathUtils.degToRad(70); // North 50 to North 40
+      } else if (targetZone == 0) {
+        return THREE.MathUtils.degToRad(40); // North 50 to North 40
+      } else if (targetZone == -2) {
+        return THREE.MathUtils.degToRad(10); // North 50 to North 40
+      }
+    }
+  };
+
+  rotateGlobeTo(targetLatLng, onComplete) {
+    const initialLatLng = this.previousTargetLatLng;
+
+    // Normalize the longitude difference for the shortest path
+    const normalizeAngle = (angle) => {
+      return ((angle + 180) % 360) - 180;
     };
 
-    animateRotation();
+    const azimuthalAngle = normalizeAngle(targetLatLng.lng - initialLatLng.lng);
+    const azimuthalRotation = THREE.MathUtils.degToRad(azimuthalAngle);
+
+    // Calculate polar rotation based on latitudinal change
+    const polarAngle = targetLatLng.lat - initialLatLng.lat;
+    const polarRotation = THREE.MathUtils.degToRad(polarAngle);
+
+    // Create quaternions for each rotation
+    const azimuthalQuaternion = new THREE.Quaternion().setFromAxisAngle(
+      new THREE.Vector3(0, 1, 0),  // Rotate around Y-axis for longitude
+      -azimuthalRotation
+    );
+
+    const polarQuaternion = new THREE.Quaternion().setFromAxisAngle(
+      new THREE.Vector3(1, 0, 0),  // Rotate around X-axis for latitude
+      polarRotation
+    );
+
+    // Combine rotations: first apply polar rotation, then azimuthal
+    const finalRotation = polarQuaternion.multiply(azimuthalQuaternion);
+
+    this.animateRotation(finalRotation, onComplete);
   }
+
+  animateRotation(finalRotation, onComplete) {
+    const duration = 2000;  // Duration of the animation in milliseconds
+    const startTime = Date.now();
+    const startRotation = this.earth.quaternion.clone();  // Clone the current rotation
+
+    const animate = () => {
+        const currentTime = Date.now();
+        const fraction = (currentTime - startTime) / duration;
+        const quaternion = new THREE.Quaternion()
+        if (fraction < 1) {
+            // Interpolating the quaternion
+            quaternion.slerp(
+                startRotation,
+                finalRotation,
+                this.earth.quaternion,
+                fraction
+            );
+            requestAnimationFrame(animate);
+        } else {
+            // Ensuring the rotation ends exactly at the target orientation
+            this.earth.quaternion.copy(finalRotation);
+            if (typeof onComplete === 'function') {
+                onComplete();
+            }
+        }
+    };
+
+    animate();
+}
+
+
 
   start() {
     this.renderer.setAnimationLoop(() => {
